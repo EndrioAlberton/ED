571199e4fd1d538c709ab93779905273d234bb2d
EndrioAlberton <96059097+EndrioAlberton@users.noreply.github.com>
2024-07-12 12:50:58 +0000

    Fix: main pilha

--- pilha/main.c
+++ pilha/main.c
@@ -1,26 +1,30 @@
-#include <stdio.h>
-#include "fila.h"
+typedef struct{
+    char nome[100];
+    char email[150];
+    char fone[15];
+} contato;
 
-int main()
-{
-    int i;
-    int r, a, b; 
-    tFila fila; 
-    inicializar(&fila);
 
-    imprimeFila(&fila);    
-    for(i=0;i<7;i++){
-        inserir(&fila, i);
-        printf("inserindo %d. ",i);
-        imprimeFila(&fila);
-    }
+typedef struct{
+	contato c;
+	struct no* prox;
+} no;
 
-    while(tamanho(&fila)>1){
-        remover(&fila,&a);
-        remover(&fila,&b);
-        r = a+b;
-        inserir(&fila, r);
-        imprimeFila(&fila); 
-    }
-    return 0;
-}
+
+typedef struct{
+     no *topo;
+} pilha;
+	
+int push (pilha *p, contato c);	// retorna 1 se o empilhamento deu certo; retorna 0 se o empilhamento deu errado
+								// recebe como parametro um contato c, os dados dessa struct devem ser copiados para o novo elemento criado
+
+int pop  (pilha *p, contato *c);//retorna 1 se a remoção deu certo, retorna 0 se deu errado;
+								//recebe como parametro um ponteiro para um contato c, que deve receber as informações removidas da pilha
+
+int top  (pilha *p, contato *c);// mesmo que o pop, mas não remove;
+
+int tamanho(pilha *p);
+
+int empty(pilha *p); //retorna 1 se está vazia, senão retorna 0;
+
+void inicializar(pilha *p);// atribui nulo para o topo da pilha
